b6623d02c9e800052e7adf87ef9faef96bc48316
Nabby <trishasalubreaquino@gmail.com>
2024-04-08 21:16:32 -0600

    Add project files.

---
+++ Lab9Startup/Components/Pages/EditBook.razor.cs
@@ -0,0 +1,40 @@
+﻿using Lab9Startup.Models;
+using Lab9Startup.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace Lab9Startup.Components.Pages
+{
+    public partial class EditBook : ComponentBase
+    {
+        private Book book = new Book();
+        private bool isSaved = false;
+
+        [Parameter]
+        public string BookId { get; set; }
+
+        // used to navigate back to the Home page
+        [Inject] NavigationManager NavigationManager { get; set; }
+
+        // book database accessor
+        BookDbAccessor bookDbAccessor = new BookDbAccessor();
+
+        protected override void OnInitialized()
+        {
+            // Get the book from the database using the BookId
+            book = bookDbAccessor.GetBook(BookId);
+        }
+
+        private async Task UpdateBook()
+        {
+            // Update the book in the database
+            bookDbAccessor.UpdateBook(book);
+
+            // Navigate back to the Home page
+            isSaved = true;
+
+            // Wait for 1 second before navigating back to the Home page
+            await Task.Delay(1000);
+            NavigationManager.NavigateTo("/");
+        }
+    }
+}
---
+++ Lab9Startup/Services/BookDbAccessor.cs
@@ -0,0 +1,103 @@
+﻿using Dapper;
+using Lab9Startup.Models;
+using MySqlConnector;
+
+namespace Lab9Startup.Services
+{
+    public class BookDbAccessor
+    {
+        protected MySqlConnection connection;
+
+        public BookDbAccessor()
+        {
+            // get environemnt variable
+            string dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+            string dbUser = Environment.GetEnvironmentVariable("DB_USER");
+            string dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = dbHost,
+                UserID = dbUser,
+                Password = dbPassword,
+                Database = "library", // Use maria db to create a database called library
+            };
+
+            connection = new MySqlConnection(builder.ConnectionString);
+        }
+
+        /// <summary>
+        /// Initialize the database and create the books table
+        /// </summary>
+        public void InitializeDatabase()
+        {
+            connection.Open();
+
+            var sql = @"CREATE TABLE IF NOT EXISTS books (
+                BookId VARCHAR(36) PRIMARY KEY,
+                Title VARCHAR(255) NOT NULL,
+                Author VARCHAR(255) NOT NULL,
+                Description TEXT,
+                Category VARCHAR(255)
+            )";
+
+            connection.Execute(sql);
+
+            connection.Close();
+        }
+
+        public void AddBook(Book book)
+        {
+            connection.Open();
+
+            string sql = "INSERT INTO books (BookId, Title, Author, Description, Category) VALUES (@BookId, @Title, @Author, @Description, @Category)";
+
+            connection.Execute(sql, book);
+            connection.Close();
+        }
+
+        public List<Book> GetBooks()
+        {
+            connection.Open();
+
+            string sql = "SELECT * FROM books";
+
+            var books = connection.Query<Book>(sql);
+
+            connection.Close();
+            return books.ToList();
+        }
+
+        public Book GetBook(string bookId)
+        {
+            connection.Open();
+
+            string sql = "SELECT * FROM books WHERE BookId = @BookId";
+
+            Book book = connection.QueryFirstOrDefault<Book>(sql, new { BookId = bookId });
+            connection.Close();
+
+            return book;
+        }
+
+        public void UpdateBook(Book book)
+        {
+            connection.Open();
+
+            string sql = "UPDATE books SET Title = @Title, Author = @Author, Description = @Description WHERE BookId = @BookId";
+
+            connection.Execute(sql, book);
+            connection.Close();
+        }
+
+        public void DeleteBook(string bookId)
+        {
+            connection.Open();
+
+            string sql = "DELETE FROM books WHERE BookId = @BookId";
+
+            connection.Execute(sql, new { BookId = bookId });
+            connection.Close();
+        }
+    }
+}
